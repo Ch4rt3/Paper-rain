8e1fe6711f6e66389a6798f6569afbcdba455764
Kristel2021 <20215161@aloe.ulima.edu.pe>
2026-06-25 22:55:23 -0500

    modificacion checkpoint

--- Assets/Script_Checkpoint/SensorMuerte.cs
+++ Assets/Script_Checkpoint/SensorMuerte.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class SensorMuerte : MonoBehaviour
 {
-    // 1. Para las zonas vacías 
+    // zonas vacías de caída 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") || (other.transform.parent != null && other.transform.parent.CompareTag("Player")))
@@ -12,13 +12,21 @@ public class SensorMuerte : MonoBehaviour
         }
     }
 
-    // 2. Para los obstáculos como manzana 
+    // 2. Para la manzana cuando choca 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Reinicia el nivel
         if (collision.gameObject.CompareTag("Player") || (collision.transform.parent != null && collision.transform.parent.CompareTag("Player")))
         {
             Debug.Log("¡La manzana aplastó a la moto!");
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+        // La manzana se destruye sola y desaparece de la pantalla
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            Debug.Log("La manzana tocó el suelo y desapareció.");
+            Destroy(gameObject); // Destruye a la manzana
         }
     }
 }
