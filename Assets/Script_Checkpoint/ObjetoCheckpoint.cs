a33e5f711a2d8b5f8047b887e0c8a28212153ce6
Mateo173 <20232356@aloe.ulima.edu.pe>
2026-07-20 16:18:16 -0500

    Corrección de materiales y fin de estrellas

--- Assets/Script_Checkpoint/ObjetoCheckpoint.cs
+++ Assets/Script_Checkpoint/ObjetoCheckpoint.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class ObjetoCheckpoint : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem checkpointStars;
+
+    private bool activado = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-         
+        if (activado) return;
+
         Debug.Log("¡Trigger detectado! Tocado por: " + other.name);
-        AudioManager.Instance.PlaySFX("checkpoint"); 
-        if (other.CompareTag("Player") || (other.transform.parent != null && other.transform.parent.CompareTag("Player")))
+
+        if (other.CompareTag("Player") ||
+            (other.transform.parent != null && other.transform.parent.CompareTag("Player")))
         {
+            activado = true;
+
+            AudioManager.Instance.PlaySFX("checkpoint");
+
             DataHolder.instance.posicionCheckpoint = transform.position;
             DataHolder.instance.tieneCheckpoint = true;
+
+            checkpointStars.Play();
+
             Debug.Log("¡Progreso guardado en el Checkpoint!");
         }
     }
-}   
+}
