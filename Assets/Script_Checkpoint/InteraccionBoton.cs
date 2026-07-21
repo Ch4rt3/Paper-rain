37a2a27b3db080760665352ecb99f9fd8855de3e
Kristel2021 <20215161@aloe.ulima.edu.pe>
2026-07-20 19:37:34 -0500

    Codigo boton

--- Assets/Script_Checkpoint/InteraccionBoton.cs
+++ Assets/Script_Checkpoint/InteraccionBoton.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class InteraccionBoton
+public class InteraccionBoton : MonoBehaviour
 {
-    
+    // Esta variable hará referencia al Rigidbody del tubo en el Inspector
+    public Rigidbody2D rigidbodyTubo;
+
+    // Esta función se ejecuta cuando algo (con un Collider 2D) entra en el Trigger del botón
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Comprobamos si lo que tocó el botón es el Jugador
+        if (collision.CompareTag("Player"))
+        {
+            // Si el tubo tiene Rigidbody2D
+            if (rigidbodyTubo != null)
+            {
+                // Cambiamos el tipo de cuerpo a Dynamic para que la gravedad actúe
+                rigidbodyTubo.bodyType = RigidbodyType2D.Dynamic;
+                
+                // Si ya era Dynamic pero con Gravity Scale 0, cámbialo aquí:
+                // rigidbodyTubo.gravityScale = 1;
+                
+                Debug.Log("¡El botón fue presionado, el tubo cae!");
+            }
+        }
+    }
 }
